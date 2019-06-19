f7656b833461fe54dcc7c266b9a6caa2e371bbce
likunkun <9865029@qq.com>
2019-06-19 15:21:02 +0800

    键值对对象

---
+++ src/com/vo/UserQueryVO.java
@@ -0,0 +1,26 @@
+package com.vo;
+
+import com.model.Order;
+import com.model.User;
+
+public class UserQueryVO {
+
+    private User user;
+
+    private Order order;
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+}
